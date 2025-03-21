7693c9276737a6087056c53c3d2675c3b2da4b6b
Trillium Smith <spiteless@gmail.com>
2025-03-21 15:53:05 -0700

    feat: Update footer with links

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <p>This website made by Trillium while rowing</p>
+      <p>
+        This website made by Trillium{" "}
+        <Link
+          className="font-bold text-primary-500 underline"
+          href="while_rowing"
+        >
+          while rowing
+        </Link>
+        .
+      </p>
     </footer>
   );
 }
