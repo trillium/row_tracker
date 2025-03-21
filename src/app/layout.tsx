f57ba3fb2c576f37490e21408e1f8696a584917e
Trillium Smith <spiteless@gmail.com>
2025-03-21 16:08:45 -0700

    refactor: Update page and footer element layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Footer />
+        <div className="grid grid-rows-[1fr_1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <div className="flex flex-col gap-8 row-start-2 items-center justify-center max-w-screen">
+            {children}
+          </div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
