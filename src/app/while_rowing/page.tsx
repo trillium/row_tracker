fbb00cf5209696a2ba630dc631648bc2e6781efe
Trillium Smith <spiteless@gmail.com>
2025-04-18 11:06:01 -0700

    feat: Update While Rowing page text

--- src/app/while_rowing/page.tsx
+++ src/app/while_rowing/page.tsx
@@ -1,14 +1,29 @@
-import Footer from "@/components/ui/footer";
-
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[1fr_1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center flex flex-row flex-grow h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center justify-center max-w-screen">
-        <div className="">
-          <p>Under construction.</p>
+        <div className="flex flex-col gap-8 p-4">
+          <p className="">
+            At the beginning of 2025, I made a commitment to myself that I would
+            row 365 times before the end of the year. Along the way I've also
+            been learning how to use the accessibility tool Talon Voice.
+          </p>
+          <p>
+            It's been my goal that all changes to this website including
+            interactions with git and GitHub happen well I'm rowing, or at the
+            very least are done through voice interaction tools directly after
+            I've completed a row.
+          </p>
+          <p>
+            Sometimes I work on this actual website while I'm rowing, sometimes
+            I watch educational videos or participate in groups like Virtual
+            Coffee or #100Devs. I wouldn't say it's the most efficient process
+            but it does keep me engaged throughout my exercise and also help me
+            stay on top of tech news and keep in touch with it friends. It's
+            been a hoot :)
+          </p>
         </div>
       </main>
-      <Footer />
     </div>
   );
 }
