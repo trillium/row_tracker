5b89b63dca7409de072d66e2b6be8081ef76cbeb
Trillium Smith <spiteless@gmail.com>
2025-04-29 08:47:34 -0700

    feat: Create introduction navbar element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/ui/footer";
+import Navbar from "@/components/ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="grid grid-rows-[1fr_1fr_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <div className="flex flex-col gap-8 row-start-2 items-center justify-center max-w-screen">
-            {children}
-          </div>
+        <div className="flex justify-between flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
+          <Navbar />
+
+          <div className="">{children}</div>
+
           <Footer />
         </div>
       </body>
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,9 @@
+// import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-900 flex flex-wrap items-center justify-center">
+      <p>This is a medium quality introduction navbar</p>
+    </nav>
+  );
+}
