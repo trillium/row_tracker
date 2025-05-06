4791046b55669df61fe2a9d6531945e1617b7a51
Trillium Smith <spiteless@gmail.com>
2025-05-05 17:39:05 -0700

    feat: Update navbar to use Link element

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,9 +1,23 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-900 flex flex-wrap items-center justify-center">
-      <p>This is a medium quality introduction navbar</p>
-    </nav>
+    <div className="w-screen flex items-center justify-center">
+      <nav className="max-w-screen-lg flex flex-grow space-between items-center justify-between p-4">
+        <Link href="/" className="text-white hover:text-gray-300">
+          Home
+        </Link>
+        <ul className="flex justify-end space-between w-full space-x-4">
+          <li>
+            <Link
+              href="/while_rowing"
+              className="text-white hover:text-gray-300"
+            >
+              While Rowing
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
