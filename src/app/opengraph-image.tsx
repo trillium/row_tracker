4c263222d7297cb9607886cf37f1f948837a8b4e
Trillium Smith <spiteless@gmail.com>
2025-05-19 18:01:10 -0700

    wip: Continued iteration on opengraph-image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,5 +1,10 @@
 import { ImageResponse } from "next/og";
-import { RowingStats } from "../components/RowingStats";
+import { countLines } from "../lib/countLines";
+import {
+  getTimezone,
+  getCurrentDateInTimezone,
+  getDaysPassedThisYear,
+} from "../lib/dateHelpers";
 
 // Image metadata
 export const alt = "Trillium's Rowing Tracker";
@@ -12,12 +17,72 @@ export const contentType = "image/png";
 
 // Image generation
 export default async function Image() {
+  const timezone = await getTimezone();
+  const today = await getCurrentDateInTimezone(timezone);
+  const rows = await countLines();
+  const daysPassedThisYear = getDaysPassedThisYear(today);
+  const rowBoolean = daysPassedThisYear - rows <= 0;
+
+  let statusDiv;
+  if (daysPassedThisYear - rows <= 0) {
+    statusDiv = (
+      <div
+        style={{
+          fontWeight: 700,
+          fontSize: 72,
+          background: "linear-gradient(to bottom, black, #22c55e, #4ade80)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          marginBottom: 24,
+        }}
+      >
+        {`Rows ahead: +${Math.abs(daysPassedThisYear - rows)}`}
+      </div>
+    );
+  } else {
+    statusDiv = (
+      <div
+        style={{
+          fontWeight: 700,
+          fontSize: 72,
+          background: "linear-gradient(to bottom, black, #ef4444, #f87171)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          marginBottom: 24,
+        }}
+      >
+        {`Rows behind: -${daysPassedThisYear - rows}`}
+      </div>
+    );
+  }
+
   return new ImageResponse(
-    <RowingStats />,
-    // ImageResponse options
+    (
+      <div
+        style={{
+          fontSize: 64,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          padding: 64,
+          border: `8px solid ${rowBoolean ? "#22c55e" : "#ef4444"}`,
+          boxSizing: "border-box",
+        }}
+      >
+        <div
+          style={{ fontWeight: 700, marginBottom: 24 }}
+        >{`Days passed this year: ${daysPassedThisYear}`}</div>
+        <div
+          style={{ fontWeight: 700, marginBottom: 24 }}
+        >{`Rows this year: ${rows}`}</div>
+        {statusDiv}
+      </div>
+    ),
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
       ...size,
     }
   );
