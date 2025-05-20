5d7e72f342c7495a177f2305bae649aa2cd6fdea
Trillium Smith <spiteless@gmail.com>
2025-05-19 18:28:49 -0700

    wip: Continued iteration on opengraph-image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -30,9 +30,11 @@ export default async function Image() {
         style={{
           fontSize: 40,
           fontWeight: 700,
-          display: "block", // changed from inline-block to block
+          display: "block",
           color: "transparent",
-          background: "linear-gradient(to bottom, black, #22c55e, #4ade80)",
+          backgroundImage:
+            "linear-gradient(to bottom, black, #22c55e, #4ade80)",
+          backgroundClip: "text",
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
           marginBottom: 24,
@@ -47,9 +49,11 @@ export default async function Image() {
         style={{
           fontSize: 40,
           fontWeight: 700,
-          display: "block", // changed from inline-block to block
+          display: "block",
           color: "transparent",
-          background: "linear-gradient(to bottom, black, #ef4444, #f87171)",
+          backgroundImage:
+            "linear-gradient(to bottom, black, #ef4444, #f87171)",
+          backgroundClip: "text",
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
           marginBottom: 24,
@@ -75,6 +79,7 @@ export default async function Image() {
           padding: 64,
           margin: 32,
           border: `8px solid ${rowBoolean ? "#22c55e" : "#ef4444"}`,
+          borderRadius: 32,
           boxSizing: "border-box",
         }}
       >
@@ -91,4 +96,56 @@ export default async function Image() {
       ...size,
     }
   );
+}
+
+{
+  /* <div
+  style={{
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
+    fontSize: 60,
+    letterSpacing: -2,
+    fontWeight: 700,
+    textAlign: "center",
+  }}
+>
+  <div
+    style={{
+      backgroundImage:
+        "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+    }}
+  >
+    Develop
+  </div>
+  <div
+    style={{
+      backgroundImage:
+        "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+    }}
+  >
+    Preview
+  </div>
+  <div
+    style={{
+      backgroundImage:
+        "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+    }}
+  >
+    Ship
+  </div>
+</div>; */
 }
