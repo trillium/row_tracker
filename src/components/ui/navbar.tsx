55ab004153d55042efd6635969cf9f8a398b352d
Trillium Smith <trillium@trilliumsmith.com>
2026-01-01 12:02:55 -0800

    feat: Add theme toggle to navbar
    Integrate ThemeSwitch component into navbar and update link colors to support dark mode with proper contrast.

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import ThemeSwitch from "@/components/ThemeSwitch";
 
 export default function Navbar() {
   return (
     <div className="w-screen flex items-center justify-center">
       <nav className="max-w-screen-lg flex flex-grow space-between items-center justify-between p-4">
-        <Link href="/" className="text-white hover:text-gray-300">
+        <Link
+          href="/"
+          className="text-black dark:text-white hover:text-gray-300"
+        >
           Home
         </Link>
-        <ul className="flex justify-end space-between w-full space-x-4">
+        <ul className="flex justify-end space-between w-full space-x-4 items-center">
           <li>
             <Link
               href="/while_rowing"
@@ -15,6 +19,9 @@ export default function Navbar() {
             >
               While Rowing
             </Link>
+          </li>
+          <li>
+            <ThemeSwitch />
           </li>
         </ul>
       </nav>
